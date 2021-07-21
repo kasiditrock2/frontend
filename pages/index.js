ba2d6565e5ec0626613237307369023fc4b3df67
kasiditrock2 <kasiditrock2@gmail.com>
2021-07-21 15:03:48 +0700

    Adminlte3

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
 import React from 'react'
-
+import AdminHeader from '../components/layouts/admin.header'
+import AdminSidebar from '../components/layouts/admin.sidebar'
+import AdminContent from '../components/layouts/admin.content'
+import AdminFooter from '../components/layouts/admin.footer'
 const Home = () => {
     return (
         <div>
-            <h1>Hello World</h1>
+            <AdminHeader />
+            <AdminSidebar />
+            <AdminContent />
+            <AdminFooter />
         </div>
     )
 }
 
-export default Home
+export default Home  
