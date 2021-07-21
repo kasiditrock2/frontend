539c6f474ef88c047ed6a704fb1e492222989123
kasiditrock2 <kasiditrock2@gmail.com>
2021-07-21 14:20:58 +0700

    Component21/7

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
+const withSass = require("@zeit/next-sass");
+
 module.exports = {
   reactStrictMode: true,
-}
+};
+// module.exports = withSass({
+//   webpack: config => {
+//       config.node = {
+//           fs: 'empty'
+//       }
+//       return config
+//   }
+// })
+
