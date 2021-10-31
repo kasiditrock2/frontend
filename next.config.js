5a9d259e6ba52fea6ab062345c4d5c5ac38fa547
kasiditrock2 <73510241+kasiditrock2@users.noreply.github.com>
2021-10-31 21:10:32 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,3 @@
-const withSass = require("@zeit/next-sass");
-
 module.exports = {
   reactStrictMode: true,
-};
-// module.exports = withSass({
-//   webpack: config => {
-//       config.node = {
-//           fs: 'empty'
-//       }
-//       return config
-//   }
-// })
-
+}
